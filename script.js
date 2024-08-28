31541edfc85e33366b509683a04499c0d6a6e10a
RaiveeChakraborty <raiveechakraborty@gmail.com>
2024-08-29 01:34:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.open-dialogue-btn').addEventListener('click', function() {
+    document.getElementById('profile-dialogue').style.display = 'flex';
+});
+
+document.querySelector('.close-dialogue-btn').addEventListener('click', function() {
+    document.getElementById('profile-dialogue').style.display = 'none';
+});
